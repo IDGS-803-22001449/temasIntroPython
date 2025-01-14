56d57b05ab3fe3cc0c5ef4e93c11a6ba04f83ffd
IDGS-803-22001449 <81856@alumnos.utleon.edu.mx>
2025-01-13 21:25:48 -0600

    Tabla de multiplicar

---
+++ tema4_while.py
@@ -0,0 +1,10 @@
+x=0
+print("Tabla a mostrar")
+num1=input('Numero1: ')
+while x <= 10:
+    res=x*int(num1)
+    print("{0} * {1} = {2}".format(num1,x,res))
+    x=x+1
+    
+
+
---
+++ temas1_variables.py
@@ -0,0 +1,11 @@
+# int Float String
+num1 = 23
+num2 = 10
+print(num1+num2)
+num1 = 'pedro'
+
+print(num1)
+
+numero3="23"
+numero4="12"
+print(int(numero3)+int(numero4))
